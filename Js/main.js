d1d9dccb86f674a4f839c1c6f3238e49e6a75c1b
kamal khatri <59205492+ZERO-KAMAL@users.noreply.github.com>
2021-07-16 21:11:03 +0545

    first commit

---
+++ Js/main.js
@@ -0,0 +1,55 @@
+// AOS.init();
+// $(window).scroll(function () {
+//     if ($(this).scrollTop() > 150) {
+//         $('.arrow').show();
+//     } else {
+//         $('.arrow').hide();
+//     }
+// });
+// ON DOM READY
+$(function () {
+  // VARIABLES
+  var rateYo = $(".rateYo");
+  var rateYoField = $("input[name='rateYoField']");
+
+  // ON CLICK ASSIGN RATING TO HIDDEN FIELD
+  $(rateYo).rateYo({
+    onSet: function (rating, rateYoInstance) {
+      $(rateYoField).val(rating);
+    },
+  });
+});
+// $(document).ready(function(){
+//   $(".owl-carousel").owlCarousel(
+
+//   );
+// });
+
+var owl = $(".owl-carousel");
+owl.owlCarousel({
+  loop: true,
+  nav: true,
+  margin: 10,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    960: {
+      items: 3,
+    },
+    1200: {
+      items: 4,
+    },
+  },
+});
+owl.on("mousewheel", ".owl-stage", function (e) {
+  if (e.deltaY > 0) {
+    owl.trigger("next.owl");
+  } else {
+    owl.trigger("prev.owl");
+  }
+  e.preventDefault();
+});
